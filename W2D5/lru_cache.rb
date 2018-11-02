83cae4585808671b6cf83d57c57da6693fcc64fb
nramabad <nramabad@gmail.com>
2018-11-01 21:36:56 -0700

    Implement this bizzaro LRU cache

---
+++ W2D5/lru_cache.rb
@@ -0,0 +1,32 @@
+class LRUCache
+
+  def initialize(size = 1)
+    @size = size
+    @cache = []
+  end
+
+  def count
+    @cache.count
+  end
+
+  def add(el)
+  if @cache.include?(el)
+     @cache.delete(el)
+     @cache << el
+   elsif count >= @size
+     @cache.shift
+     @cache << el
+   else
+     @cache << el
+   end
+  end
+
+  def show
+     print @cache.reverse
+     nil
+  end
+
+  private
+   # helper methods go here!
+
+ end
